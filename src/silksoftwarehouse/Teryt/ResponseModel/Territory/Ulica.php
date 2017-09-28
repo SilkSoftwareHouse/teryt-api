47caa2b2b6cf245e1fa52530f84aac7c9a8dea0b
Maciej Gierej <maciej.gierej@silksh.pl>
2017-09-28 12:21:56 +0200

    Fixed assigns to model properties

--- src/silksoftwarehouse/Teryt/ResponseModel/Territory/Ulica.php
+++ src/silksoftwarehouse/Teryt/ResponseModel/Territory/Ulica.php
@@ -67,20 +67,20 @@ class Ulica extends AbstractResponseModel
     {
         if ($oData) {
             $this->streetIdentity = $oData->Cecha;
-            $this->communeTypeId  = isset($oData->GmiRodzaj) ?: isset($oData->RodzGmi) ?: null;
-            $this->communeTypeId  = isset($oData->GmiRodzaj) ?: isset($oData->RodzGmi) ?: null;
-            $this->communeId      = isset($oData->GmiSymbol) ?: isset($oData->Gmi) ?: null;
-            $this->communeName    = isset($oData->Gmina) ?: null;
-            $this->cityId         = isset($oData->IdentyfikatorMiejscowosci);
-            $this->streetId       = isset($oData->IdentyfikatorUlicy) ?: isset($oData->SymbolUlicy) ?: null;
-            $this->streetName     = isset($oData->Nazwa) ?: null;
-            $this->cityName       = isset($oData->NazwaMiejscowosci) ?: null;
-            $this->districtId     = isset($oData->PowSymbol) ?: isset($oData->Pow) ?: null;
-            $this->districtName   = isset($oData->Powiat) ?: null;
-            $this->provinceId     = isset($oData->WojSymbol) ?: isset($oData->Woj) ?: null;
-            $this->provinceName   = isset($oData->Wojewodztwo) ?: null;
+            $this->communeTypeId  = isset($oData->GmiRodzaj) ? $oData->GmiRodzaj : isset($oData->RodzGmi) ? $oData->RodzGmi : null;
+            $this->communeTypeId  = isset($oData->GmiRodzaj) ? $oData->GmiRodzaj : isset($oData->RodzGmi) ? $oData->RodzGmi : null;
+            $this->communeId      = isset($oData->GmiSymbol) ? $oData->GmiSymbol : isset($oData->Gmi) ? $oData->Gmi : null;
+            $this->communeName    = isset($oData->Gmina) ? $oData->Gmina : null;
+            $this->cityId         = isset($oData->IdentyfikatorMiejscowosci) ? $oData->IdentyfikatorMiejscowosci : null;
+            $this->streetId       = isset($oData->IdentyfikatorUlicy) ? $oData->IdentyfikatorUlicy : isset($oData->SymbolUlicy) ? $oData->SymbolUlicy : null;
+            $this->streetName     = isset($oData->Nazwa) ? $oData->Nazwa : null;
+            $this->cityName       = isset($oData->NazwaMiejscowosci) ? $oData->NazwaMiejscowosci : null;
+            $this->districtId     = isset($oData->PowSymbol) ? $oData->PowSymbol : isset($oData->Pow) ? $oData->Pow : null;
+            $this->districtName   = isset($oData->Powiat) ? $oData->Powiat : null;
+            $this->provinceId     = isset($oData->WojSymbol) ? $oData->WojSymbol : isset($oData->Woj) ? $oData->Woj : null;
+            $this->provinceName   = isset($oData->Wojewodztwo) ? $oData->Wojewodztwo : null;
 
-            $dataStanu = isset($oData->DataStanu) ?: isset($oData->StanNa) ?: null;
+            $dataStanu = isset($oData->DataStanu) ? $oData->DataStanu : isset($oData->StanNa) ? $oData->StanNa : null;
             try {
                 $this->statusDate = $dataStanu ? (new \DateTime($dataStanu))->format('Y-m-d') : null;
             } catch (\Exception $e) {
